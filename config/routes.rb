916220e8b846e377cadd7b2e3fe87c6ef375b9da
DTIC. Herrera Ordonez Fernando Jose <fjherrera@eeq.com.ec>
2023-03-11 20:48:21 -0500

    Set credentials

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_scope :user do
+    post '/signup', to: 'registrations#create'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end 
 end
